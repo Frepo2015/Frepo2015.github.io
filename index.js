f0586b85f091cf84b631dc552192f3b2ca3c38e6
Alfredo Córdova Ríos <70955489+Frepo2015@users.noreply.github.com>
2023-03-23 14:44:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,6 @@ function Verify() {
         alert("Usuario no encontrado");
     }
 }
-
-    document.getElementById('showU').addEventListener('click', Show('divUsers'))
     document.querySelector('form').addEventListener('submit', e=>{
         e.preventDefault()
         const data = Object.fromEntries(
